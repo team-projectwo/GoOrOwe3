8c47f7134ffa64068fb57920a26e862ec58c70c7
Anthony Ngu <anthonyngu@anthonys-mbp.uconnect.utah.edu>
2018-09-18 19:05:03 -0600

    parallax, buttons centered to page

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
-import Signin from "./pages/Signin"
+import Signin from "./pages/Signin";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-
-
 
 const App = () => (
   <Router>
     <div>
-      <Nav />
       <Switch>
         <Route exact path="/" component={Books} />
         <Route exact path="/books" component={Books} />
