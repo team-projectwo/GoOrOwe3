2570f5b3db4e725bd65eb1f91a31e52b393f55c7
ZCOLT <colton.zac@gmail.com>
2018-09-18 20:25:34 -0600

    fixed route import from 'groupsController'

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
+const booksController = require("../../controllers/groupsController");
 
 // Matches with "/api/books"
 router.route("/")
