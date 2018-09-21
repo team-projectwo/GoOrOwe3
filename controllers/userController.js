175b4f6bc7d86131a69acc11c636347baf009d24
ZCOLT <colton.zac@gmail.com>
2018-09-21 14:27:05 -0600

    working on user auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,34 @@
-const db = require("../models/group");
+const db = require("../models/user");
 
-// Defining methods for the GroupsController
+// Defining methods for the usersController
 module.exports = {
-  findAll: function(req, res) {
-    db.Group
-      .find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  findById: function(req, res) {
-    db.Group
-      .findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
+//   findAll: function(req, res) {
+//     db.User
+//       .find(req.query)
+//       .sort({ date: -1 })
+//       .then(dbModel => res.json(dbModel))
+//       .catch(err => res.status(422).json(err));
+//   },
+//   findById: function(req, res) {
+//     db.User
+//       .findById(req.params.id)
+//       .then(dbModel => res.json(dbModel))
+//       .catch(err => res.status(422).json(err));
+//   },
   create: function(req, res) {
-    db.Group
+    db.User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Group
+    db.User
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Group
+    db.User
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
