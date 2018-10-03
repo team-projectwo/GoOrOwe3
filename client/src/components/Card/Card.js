069a5ff9a10f5f29f0a84df0acd5131346d73855
parker grant <parkeragrant@gmail.com>
2018-10-02 19:46:20 -0600

    better groups page and card

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import { Parallax, Button, Row, Col, CardTitle } from "react-materialize";
 
+var cardStyle = {
+    // display: 'flex',
+    minHeight: '425px',
+    padding: "20px"
+
+}
+
 class Card extends Component {
     // state = {
     //     group: null
     // };
-    
+
     render() {
         console.log(this.props)
         return (
-            <Col m={6} s={12}>
-                <div className='card content blue-grey darken-3 white-text'>  <span calssName='card-title'><h4>{this.props.title}</h4></span>
+            <Col m={12}>
+                <div style={cardStyle} className='card content blue-grey darken-3 white-text z-depth-5'>  <span className='card-title'><h1>{this.props.title}</h1></span>
 
-                    <h6>{this.props.children}</h6>
+                    <h3>{this.props.children}</h3>
 
                     <div className="card-action">
-                        <a href={'/groups/' + this.props._id}><Button waves>See Group</Button></a>
+                        <a href={'/groups/' + this.props._id}><Button className="btn-large" waves>See Group</Button></a>
                     </div>
                 </div>
             </Col>
